05e9ef7aa3383ef5540e2ffd5ee72f3b2b3bbfb2
elshad.sharbatov <elshad.sharbatov@cybernet.az>
2021-09-20 15:54:34 +0400

    a

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         int j = 0;
-        String subject = "", subject1 = "";
+        String subjectinsert = "", subjectfind = "";
         List<String> list = new ArrayList<>();
 
         Scanner scanner = new Scanner(System.in);
@@ -14,14 +14,14 @@ public class Main {
         System.out.println("Sözləri daxil edin:");
 
         for (int i = 0; i < j; i++) {
-            subject = scanner.next();
-            list.add(subject);
+            subjectinsert = scanner.next();
+            list.add(subjectinsert);
         }
 
         System.out.println("Hansı sözü çapa verəcəksən ?");
-        subject1 = scanner.next();
+        subjectfind = scanner.next();
         System.out.println("Sözlər daxil edildi,Databese-dən axtarış edilir...");
-        fooOptional(list, subject1).ifPresent(System.out::println);
+        fooOptional(list, subjectfind).ifPresent(System.out::println);
 
     }
 
@@ -31,7 +31,7 @@ public class Main {
             if (word.equalsIgnoreCase(findword))
                 return Optional.of(word);
         }
-        return Optional.of("Tapılmadı");
+        return Optional.of("Daxil etdiyin söz tapılmadı");
     }
 
 }
